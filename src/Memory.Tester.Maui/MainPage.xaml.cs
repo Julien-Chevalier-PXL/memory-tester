e9512103d478237eb04c3813fabba6da9ecef5f7
Julien C. <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2026-04-10 23:02:24 +0200

    [INT] Init projects

---
+++ src/Memory.Tester.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Memory.Tester.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
