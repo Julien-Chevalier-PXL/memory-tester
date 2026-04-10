706c0927e838eed377757e87b66be096d4660843
Julien C. <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2026-04-10 23:17:54 +0200

    Data - Add Question entity

---
+++ src/Memory.Tester.Core.Data/Entities/Abstractions/AEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Memory.Tester.Core.Data.Entities.Abstractions;
+
+/// <summary>
+/// Abstract class representing an entity.
+/// </summary>
+public abstract class AEntity
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the entity.
+    /// </summary>
+    public int Id { get; set; }
+}
---
+++ src/Memory.Tester.Core.Data/Entities/Question.cs
@@ -0,0 +1,19 @@
+﻿namespace Memory.Tester.Core.Data.Entities;
+
+using Memory.Tester.Core.Data.Entities.Abstractions;
+
+/// <summary>
+/// Class representing a question.
+/// </summary>
+public sealed class Question : AEntity
+{
+    /// <summary>
+    /// Gets or sets the title of the question.
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the answer of the question.
+    /// </summary>
+    public string Answer { get; set; } = string.Empty;
+}
