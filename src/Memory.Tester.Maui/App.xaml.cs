23aca8d8b941396cc6d7eba0b029f9bfda99c9c3
Julien C. <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2026-04-12 15:14:25 +0200

    [INT] Create publication profile

--- src/Memory.Tester.Maui/App.xaml.cs
+++ src/Memory.Tester.Maui/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace Memory.Tester.Maui
+namespace Memory.Tester.Maui
 {
     public partial class App : Application
     {
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "Memory.Tester.Maui" };
+            return new Window(new MainPage()) { Title = "Memory Tester" };
         }
     }
 }
